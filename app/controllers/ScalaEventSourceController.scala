6fbb9346d2b32c56b3875ee5379355176aabd2c1
juan <juancamilogaviriaacosta@gmail.com>
2017-07-28 00:09:16 -0500

    archivos

--- app/controllers/ScalaEventSourceController.scala
+++ app/controllers/ScalaEventSourceController.scala
@@ -1,7 +1,8 @@
 package controllers
 
+import play.Play
+import java.nio.file.Paths
 import javax.inject.{Inject, Singleton}
-
 import play.api.http.ContentTypes
 import play.api.libs.EventSource
 import play.api.mvc._
@@ -15,6 +16,19 @@ class ScalaEventSourceController @Inject()(cc: ControllerComponents) extends Abs
 
   def streamClock() = Action {
     Ok.chunked(stringSource via EventSource.flow).as(ContentTypes.EVENT_STREAM)
+  }
+
+  def upload = Action(parse.multipartFormData) { request =>
+    request.body.file("picture").map { picture =>
+      val basePath = Play.application.path.getPath + "/tmp/"
+      val filename = picture.filename
+      val contentType = picture.contentType
+      picture.ref.moveTo(Paths.get(basePath + filename), replace = true)
+      Ok(views.html.scalaeventsource())
+    }.getOrElse {
+      Redirect(routes.ScalaEventSourceController.index).flashing(
+        "error" -> "Missing file")
+    }
   }
 
 }
